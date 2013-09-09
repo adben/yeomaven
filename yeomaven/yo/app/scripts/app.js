78c01f53528b8cf925fc9fd8b9df55a78033d36b
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2013-09-09 16:05:01 +0200

    Linting: fixed indentations

--- yeomaven/yo/app/scripts/app.js
+++ yeomaven/yo/app/scripts/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('yoApp', ['ngResource'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

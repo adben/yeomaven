5ea68a5a265450e28b1c7e83685f9d0bb9f57b36
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2013-09-09 16:12:43 +0200

    Linting: fixed test

--- yeomaven/yo/app/scripts/controllers/main.js
+++ yeomaven/yo/app/scripts/controllers/main.js
@@ -2,6 +2,11 @@
 
 angular.module('yoApp')
   .controller('MainCtrl', function ($scope, $resource) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
     var Book = $resource('./rest/books/:isbn');
     $scope.books = Book.query();
   });

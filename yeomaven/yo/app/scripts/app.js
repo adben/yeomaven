fd00347347e91b6ec6f7e5458eb67dfd9449e2a3
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2013-09-09 15:37:03 +0200

    fe: updated angular application and controller

--- yeomaven/yo/app/scripts/app.js
+++ yeomaven/yo/app/scripts/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module('yoApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+angular.module('yoApp', ['ngResource'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
